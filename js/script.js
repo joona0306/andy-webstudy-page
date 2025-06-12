f2645377a030855883e3c4e6c576b9984204c87a
joona0306 <dev.joona0306@gmail.com>
2025-06-12 18:12:08 +0900

    검색기능, 고탑 버튼 기능 구현

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 const wrap = document.querySelector(".card-wrap");
+const searchInput = document.querySelector(".search-input");
+const searchBtn = document.querySelector(".search-btn");
+// 초기 filterInput 데이터
+let filterInput = "";
 // 초기 데이터 리스트
 let dataList = [];
 
@@ -13,47 +17,44 @@ fetch("data/data.json")
   dataList = data;
 
   // 카드 목록 렌더링 함수 호출
-  renderCardList();
+  renderCardList(dataList);
 
-  // isotope 인스턴스 객체 생성
-  const cardWrapIso = new Isotope(wrap, {
-   itemSelector: ".card",
-   masonry: {
-    // use element for option
-    columnWidth: ".card",
-   },
+  // 검색어 입력
+  searchInput.addEventListener("input", (e) => {
+   //  console.log(e.target.value);
+   filterInput = e.target.value
+    .toLowerCase()
+    .replace(/[^ㄱ-ㅎ가-힣a-z0-9]+/g, "");
   });
-
-  // imagesloaded를 활용하여 모든 이미지 로드 완료 후 레이아웃 재계산
-  const cardImgLoad = imagesLoaded(wrap);
-  // 모든 이미지 로드 완료 시 실행
-  cardImgLoad.on("done", function (instance) {
-   console.log("모든 이미지 로드 완료!");
-   // 레이아웃 재계산
-   cardWrapIso.layout();
-  });
-  // 이미지 로드 실패 시 실행
-  cardImgLoad.on("fail", function (instance) {
-   console.log("일부 이미지 로드 실패!");
-   // 레이아웃 재계산
-   cardWrapIso.layout();
-  });
-
-  // 필터 기능
-  const filterBtnList = document.querySelectorAll(".filter li");
-  // console.log(filterBtnList);
-
-  filterBtnList.forEach((filterBtn) => {
-   filterBtn.addEventListener("click", (e) => {
-    e.preventDefault(); // a태그 기본동작 방지
-
-    // 필터링 및 재정렬
-    const filter = e.currentTarget.querySelector("a").getAttribute("href");
-
-    cardWrapIso.arrange({
-     filter: filter,
-    });
+  // 검색 결과
+  searchInput.addEventListener("keyup", (e) => {
+   const filterCardList = dataList.filter((data) => {
+    return data.title
+     .toLowerCase()
+     .replace(/[^ㄱ-ㅎ가-힣a-z0-9]+/g, "")
+     .includes(filterInput);
    });
+
+   // 엔터키일 때만 카드 렌더링
+   if (e.keyCode === 13) {
+    renderCardList(filterCardList);
+
+    // 텍스트필드 초기화
+    searchInput.value = "";
+   }
+  });
+  searchBtn.addEventListener("click", () => {
+   const filterCardList = dataList.filter((data) => {
+    return data.title
+     .toLowerCase()
+     .replace(/[^ㄱ-ㅎ가-힣a-z0-9]+/g, "")
+     .includes(filterInput);
+   });
+
+   renderCardList(filterCardList);
+
+   // 텍스트필드 초기화
+   searchInput.value = "";
   });
  })
  // 요청에 실패하면
@@ -62,7 +63,7 @@ fetch("data/data.json")
  });
 
 // 카드 목록 렌더링 함수
-function renderCardList() {
+function renderCardList(dataList) {
  const cardList = dataList
   .map((card, index) => {
    return `
@@ -79,4 +80,60 @@ function renderCardList() {
 
  wrap.innerHTML = cardList;
  //  console.log(cardList);
+
+ // isotope 인스턴스 객체 생성
+ const cardWrapIso = new Isotope(wrap, {
+  itemSelector: ".card",
+  transitionDuration: "0.8s",
+  masonry: {
+   // use element for option
+   columnWidth: ".card",
+  },
+ });
+
+ // imagesloaded를 활용하여 모든 이미지 로드 완료 후 레이아웃 재계산
+ const cardImgLoad = imagesLoaded(wrap);
+ // 모든 이미지 로드 완료 시 실행
+ cardImgLoad.on("done", function (instance) {
+  console.log("모든 이미지 로드 완료!");
+  // 레이아웃 재계산
+  cardWrapIso.layout();
+ });
+ // 이미지 로드 실패 시 실행
+ cardImgLoad.on("fail", function (instance) {
+  console.log("일부 이미지 로드 실패!");
+  // 레이아웃 재계산
+  cardWrapIso.layout();
+ });
+
+ // 필터 기능
+ const filterBtnList = document.querySelectorAll(".filter li");
+ // console.log(filterBtnList);
+
+ filterBtnList.forEach((filterBtn) => {
+  filterBtn.addEventListener("click", (e) => {
+   e.preventDefault(); // a태그 기본동작 방지
+
+   // 필터링 및 재정렬
+   const filter = e.currentTarget.querySelector("a").getAttribute("href");
+
+   cardWrapIso.arrange({
+    filter: filter,
+   });
+  });
+ });
 }
+
+// go top button
+const goTopBtn = document.querySelector(".go-top-btn");
+window.addEventListener("scroll", () => {
+ if (window.scrollY <= 0) {
+  goTopBtn.style.visibility = "hidden";
+ } else {
+  goTopBtn.style.visibility = "visible";
+ }
+});
+
+goTopBtn.addEventListener("click", () => {
+ window.scrollTo({ top: 0 });
+});
