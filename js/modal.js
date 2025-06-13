b4d995e5e2677bba8a3165b835dbb0d07c3eb83d
joona0306 <dev.joona0306@gmail.com>
2025-06-14 00:37:37 +0900

    feat: 프로젝트 설명 모달 기능 구현

--- js/modal.js
+++ js/modal.js
@@ -1,38 +1,43 @@
 const modal = document.querySelector("#modal");
 const modalCloseBtn = document.querySelector("#modal .text span");
-const cardList = document.querySelectorAll(".card");
+
 let cardDataList = [];
 
 fetch("data/data.json")
- .then((response) => {
-  return response.json();
- })
- .then((data) => {
-  cardDataList = data;
-  console.log(cardDataList);
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    cardDataList = data;
+    // console.log(cardDataList);
 
-  cardDataList.forEach((card) => {
-   card.addEventListener("click", (e) => {
-    console.log("click");
-    const title = e.currentTarget.querySelector("h2").textContent;
-    const desc = e.currentTarget.querySelector("p").textContent;
-    const imgSrc = e.currentTarget.querySelector("img").getAttribute("src");
+    const cardList = document.querySelectorAll(".card");
 
-    modal.querySelector("h2").textContent = title;
-    modal.querySelector("p").textContent = desc;
-    modal.querySelector("img").setAttribute("src", imgSrc);
+    cardList.forEach((card) => {
+      card.addEventListener("click", (e) => {
+        console.log("click");
+        const title = e.currentTarget.querySelector("h2").textContent;
+        const desc = e.currentTarget.querySelector("p").textContent;
+        const imgSrc = e.currentTarget.querySelector("img").getAttribute("src");
 
-    modal.classList.add("active");
-    // document.body.style.overflow = "hidden";
-   });
+        modal.querySelector("h2").textContent = title;
+        modal.querySelector("p").textContent = desc;
+        modal.querySelector("img").setAttribute("src", imgSrc);
+
+        modal.classList.add("active");
+
+        // go top button
+        const goTopBtn = document.querySelector(".go-top-btn");
+        // console.log(goTopBtn);
+        goTopBtn.style.display = "none";
+      });
+    });
+
+    modalCloseBtn.addEventListener("click", () => {
+      modal.classList.remove("active");
+      goTopBtn.style.display = "flex";
+    });
+  })
+  .catch((err) => {
+    console.log(err);
   });
-
-  modalCloseBtn.addEventListener("click", () => {
-   modal.classList.remove("active");
-   //  document.body.style.overflow = "none";
-   //  document.body.style.overflowX = "hidden";
-  });
- })
- .catch((err) => {
-  console.log(err);
- });
